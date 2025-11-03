3beb227ecefac7f786300bed67051de568941b85
takeruhiura <157721288+takeruhiura@users.noreply.github.com>
2025-11-03 16:34:14 -0500

    Update top_module_tb.v

--- top_module_tb.v
+++ top_module_tb.v
@@ -1,6 +1,28 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/03/2025 04:17:27 PM
+// Design Name: 
+// Module Name: top_module_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module top_module_tb;
+
+`timescale 1ns / 1ps
+
+module top_module_tb();
 
     reg mclk;
     wire [6:0] seg;
